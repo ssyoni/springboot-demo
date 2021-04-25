a2be7d494046e4bd810e234d2d0be37213470baa
ssyoni <jsy950601@gmail.com>
2021-04-25 22:02:34 +0900

    springboot test

--- src/main/java/com/demo/study/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/demo/study/springbootdemo/SpringbootDemoApplication.java
@@ -2,11 +2,23 @@ package com.demo.study.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringbootDemoApplication {
+	/* @RestController : @Controller와 @ResponseBody를 합쳐놓은 역할을 수행. 어노테이션을 사용하면 스프링은 반환값이 Response Body 부분에 자동 바인딩 된다. 
+						 스프링에서 RESTful 웹 서비스를 만들 때 주로 사용됨.
+	   @GetMapping : Get 방식으로 경로를 받는 매핑 어노테이션. value값 별도지정 없을 경우 기본값인 빈 값("")이 들어간다.
+	*/
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDemoApplication.class, args);
+	}
+
+	@GetMapping
+	public String HelloWorld(){
+		return "Hello World";
 	}
 
 }
--- src/test/java/com/demo/study/springbootdemo/SpringbootDemoApplicationTests.java
+++ src/test/java/com/demo/study/springbootdemo/SpringbootDemoApplicationTests.java
@@ -1,13 +1,46 @@
 package com.demo.study.springbootdemo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.List;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SpringbootDemoApplicationTests {
+	@Value("${property.test.name}")
+	private String propertyTestName;
+
+	@Value("${propertyTest}")
+	private String propertyTest;
+
+	@Value("${noKey:default value}")
+	private String defaulValue;
+
+	@Value("${propertyTestList}")
+	private String[] propertyTestArray;
+
+	@Value("#{'${propertyTestList}'.split(',')}")
+	private List<String> propertyTestList;
 
 	@Test
-	void contextLoads() {
+	public void testValue() {
+		assertThat(propertyTestName, is("property depth test"));
+		assertThat(propertyTest, is("test"));
+		assertThat(defaulValue, is("default value"));
+
+		assertThat(propertyTestArray[0], is("a"));
+		assertThat(propertyTestArray[1], is("b"));
+		assertThat(propertyTestArray[2], is("c"));
+
+		assertThat(propertyTestList.get(0), is("a"));
+		assertThat(propertyTestList.get(1), is("b"));
+		assertThat(propertyTestList.get(2), is("c"));
 	}
 
 }
