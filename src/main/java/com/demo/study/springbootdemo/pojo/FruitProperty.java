9b8ea731644eb653604d4eb0faadc475f69f75a9
ssyoni <jsy950601@gmail.com>
2021-05-01 23:16:04 +0900

    @configurationProperties

--- src/main/java/com/demo/study/springbootdemo/pojo/FruitProperty.java
+++ src/main/java/com/demo/study/springbootdemo/pojo/FruitProperty.java
@@ -6,9 +6,19 @@ import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import lombok.Data;
+
+@Data
 @Component
-@ConfigurationProperties("fruit")
+@ConfigurationProperties("fruit") 
 public class FruitProperty {
-    private List<Map> list;
+    /*
+    @ConfigurationProperties
+     - 접두사를 활용하여 원하는 객체를 바인딩 해준다. @Value보다 더 객체지향적으로 프로퍼티를 매핑한다. 
+     - @ConfigurationProperties를 사용하려면 해당 클래스를 @Component로 선언해야 의존성 주입이 가능하다.
+     - application.yml이 아는 다른 이름의 YAML파일을 따로 생성해서 관리할 때는 
+        @ConfigurationProperties(prefix = "fruit")
+    */
+    private List<Fruit> list;
 
 }
---
+++ src/test/java/com/demo/study/springbootdemo/pojo/PropertyTest.java
@@ -0,0 +1,36 @@
+package com.demo.study.springbootdemo.pojo;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PropertyTest {
+    
+    @Autowired
+    FruitProperty fruitProperty;
+
+    @Test
+    public void test(){
+        //List<Map> fruitData = fruitProperty.getList();
+        List<Fruit> fruitData = fruitProperty.getList();
+
+        assertThat(fruitData.get(0).getName(), is("banana"));
+        assertThat(fruitData.get(0).getColor(), is("yellow"));
+        
+        assertThat(fruitData.get(1).getName(), is("apple"));
+        assertThat(fruitData.get(1).getColor(), is("red"));
+        
+        assertThat(fruitData.get(2).getName(), is("water melon"));
+        assertThat(fruitData.get(2).getColor(), is("green"));
+    }
+
+}
